46f77ee5a7b8bec9f766db4ff726fa30ab17c04a
HarshitChandel02 <chandelharshit02@gmail.com>
2024-07-25 09:37:20 +0530

    Create Que1.cpp

---
+++ Que1.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        s += arr[i];
+    }
+
+    cout << s / n << endl;
+}
